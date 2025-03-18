ead4d81e3898c3378dbdc32d0a9c3b3890e7e0e5
Ali-2152004 <112330406+Ali-2152004@users.noreply.github.com>
2025-03-18 02:15:04 +0200

    create note model

---
+++ lib/models/note_model.dart
@@ -0,0 +1,13 @@
+class NoteModel {
+  final String title;
+  final String subTitle;
+  final String data;
+  final int color;
+
+  NoteModel({
+    required this.color,
+    required this.data,
+    required this.title,
+    required this.subTitle,
+  });
+}
